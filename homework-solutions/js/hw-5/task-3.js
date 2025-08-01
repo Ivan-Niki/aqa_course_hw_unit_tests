3a2addafd80215cc471b8bc628ffccad9d686ec6
Ivan-Niki <iFankov@gmail.com>
2025-08-01 15:43:41 +0300

    added hw-5 to js-3 branch

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,7 +4,18 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
-
+const lowerWord = word.toLowerCase();
+let vowelsCount = 0;
+let consonantsCount = 0;
 let vowelsAndConsonantsResult = '';
+for (let i = 0; i < lowerWord.length; i++) {
+  if (lowerWord[i] === 'a' || lowerWord[i] === 'e' || lowerWord[i] === 'i' || lowerWord[i] === 'o' || lowerWord[i] === 'u' || lowerWord[i] === 'y') {
+    vowelsCount++
+  } else {
+    consonantsCount++
+  }
+}
 
+vowelsAndConsonantsResult += `${word} contains ${vowelsCount} vowels and ${consonantsCount} consonants`;
+// console.log(vowelsAndConsonantsResult);
 export { vowelsAndConsonantsResult };
