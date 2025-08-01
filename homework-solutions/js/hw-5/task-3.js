9fb8fdf2267e249a7466b535ef1c8c40fb5c23b9
Ivan-Niki <iFankov@gmail.com>
2025-08-01 22:23:32 +0300

    fixed hw-5-task-3 on js-3 branch

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -5,13 +5,16 @@
  */
 const word = 'hello';
 const lowerWord = word.toLowerCase();
+const vowelsList = 'aeiouy';
+const consonantsList = 'bcdfghjklmnpqrstvwxz';
 let vowelsCount = 0;
 let consonantsCount = 0;
 let vowelsAndConsonantsResult = '';
+
 for (let i = 0; i < lowerWord.length; i++) {
-  if (lowerWord[i] === 'a' || lowerWord[i] === 'e' || lowerWord[i] === 'i' || lowerWord[i] === 'o' || lowerWord[i] === 'u' || lowerWord[i] === 'y') {
+  if (vowelsList.includes(lowerWord[i])) {
     vowelsCount++
-  } else {
+  } else if (consonantsList.includes(lowerWord[i])) {
     consonantsCount++
   }
 }
