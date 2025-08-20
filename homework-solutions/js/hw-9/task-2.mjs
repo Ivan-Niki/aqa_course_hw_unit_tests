5165670fa0be694afef8df0a2217cdc04936cea0
Ivan-Niki <iFankov@gmail.com>
2025-08-20 18:49:38 +0300

    added HW-9-solutions js

--- homework-solutions/js/hw-9/task-2.mjs
+++ homework-solutions/js/hw-9/task-2.mjs
@@ -15,22 +15,57 @@ const characters = [
 
 function addCharacter(character) {
   // Ваш код
+  if (!('name' in character && 'age' in character && typeof character.name === 'string' && typeof character.age === 'number')) {
+    throw new Error('Invalid input')
+  } else {
+    characters.push(character)
+  }
 }
+// && Object.keys(characters).length === 2
+// && typeof character === 'object'
+addCharacter({ name: 'Ivan', age: 37 });
+console.log(characters);
 
 function getCharacter(name) {
   // Ваш код
+  const resultcharacter = characters.find((character) => character.name === name);
+  return resultcharacter;
 }
+// console.log(getCharacter('Barney'));
+
 
 function getCharactersByAge(minAge) {
   // Ваш код
+  if (typeof minAge !== 'number') {
+    throw new Error('Invalid input')
+  }
+  const resultcharacterArr = characters.filter((character) => character.age >= minAge);
+  return resultcharacterArr
 }
+// if (resultcharacterArr.length > 0)
+// console.log(getCharactersByAge(39));
+
 
 function updateCharacter(name, newCharacter) {
   // Ваш код
+  let targetChar = getCharacter(name);
+  targetChar.name = newCharacter.name;
+  targetChar.age = newCharacter.age;
 }
+// updateCharacter('Fred', { name: 'Tom', age: 21 });
+// console.log(characters);
 
 function removeCharacter(name) {
   // Ваш код
+  let charIndex = characters.findIndex((character) => character.name === name);
+  if (charIndex === -1) {
+    throw new Error(`Character is not found`)
+  } else {
+    characters.splice(charIndex, 1)
+  }
 }
+// removeCharacter('Barney');
+// console.log('characters after removing ===> ', characters);
+
 
 export { characters, addCharacter, updateCharacter, getCharacter, getCharactersByAge, removeCharacter };
